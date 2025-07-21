8e439b4ab0c8610269d902ec77dd36f4404eeefa
Indexc <indexc@gmail.com>
2025-07-21 20:31:41 +0300

    killer, player hp

--- Killer/setup.mjs
+++ Killer/setup.mjs
@@ -1,5 +1,7 @@
 export async function setup({settings, onCharacterLoaded, patch}) {
 
+    const _events = mitt();
+
     settings.section('General').add({
         type: 'switch',
         label: 'Bosses only',
@@ -22,6 +24,13 @@ export async function setup({settings, onCharacterLoaded, patch}) {
         default: false,
     });
     settings.section('General').add({
+        type: 'switch',
+        label: 'Player HP',
+        hint: 'Double player HP value',
+        name: 'playerHP',
+        default: false,
+    });
+    settings.section('General').add({
         type: 'number',
         label: 'Max and min hit from HP %',
         hint: 'Set min/max hit values - formula: original min/max hit value + % from HP, default: 0',
@@ -34,6 +43,13 @@ export async function setup({settings, onCharacterLoaded, patch}) {
         hint: 'Set flat barrier damage value - formula: original flatBarrierDamage modifier + % current barrier value, default: 0',
         name: 'barrierValue',
         default: 0,
+    });
+    settings.section('General').add({
+        type: 'number',
+        label: 'x times to double player HP',
+        hint: 'Set how many times we doubling player HP, default: 1',
+        name: 'doublePlayerHPTimes',
+        default: 1,
     });
 
 
@@ -57,6 +73,24 @@ export async function setup({settings, onCharacterLoaded, patch}) {
                 let oldMaxHit = game.combat.player.stats.summoningMaxHit;
 
                 game.combat.player.stats.summoningMaxHit =  newMaxHit > oldMaxHit ? newMaxHit : oldMaxHit;
+            }
+
+            if (settings.section('General').get('playerHP')) {
+                let times = settings.section('General').get('doublePlayerHPTimes');
+
+                let oldMax = game.combat.player.stats.maxHitpoints;
+                let oldCurrent = game.combat.player.hitpoints;
+                let newMax = oldMax * times;
+                let newCurrent = oldCurrent * times;
+
+                game.combat.player.stats.maxHitpoints = newMax;
+                if (newCurrent >= newMax) {
+                    game.combat.player.setHitpoints(newMax);
+                } else {
+                    game.combat.player.setHitpoints(newCurrent);
+                }
+
+                _events.emit('hitpointsChanged', new HitpointsChangedEvent(oldCurrent, oldMax, game.combat.player.hitpoints, game.combat.player.stats.maxHitpoints));
             }
         });
     });
